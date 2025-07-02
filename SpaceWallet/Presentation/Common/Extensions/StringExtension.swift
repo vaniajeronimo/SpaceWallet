f0aed7408f016b572fc79df436046d8aad6e8091
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-02 11:17:11 +0100

    Add localizable strings file to project.

---
+++ SpaceWallet/Presentation/Common/Extensions/StringExtension.swift
@@ -0,0 +1,15 @@
+//
+//  StringExtension.swift
+//  SpaceWallet
+//
+//  Created by Vania Jeronimo on 02/07/2025.
+//
+
+import Foundation
+
+extension String {
+
+	var localized: String {
+		NSLocalizedString(self, bundle: Bundle.main, comment: "")
+	}
+}
