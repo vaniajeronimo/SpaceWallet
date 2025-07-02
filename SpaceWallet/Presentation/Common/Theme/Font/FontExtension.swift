5dd829e59cc91a4f3e318296b26b08a0ad95090d
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-02 18:25:33 +0100

    Fix fonts.

--- SpaceWallet/Presentation/Common/Theme/Font/FontExtension.swift
+++ SpaceWallet/Presentation/Common/Theme/Font/FontExtension.swift
@@ -24,25 +24,18 @@ extension Font {
 			self.letterSpacing = letterSpacing
 		}
 
-		public static let displayDisplayRegular = Self(SFProFont.sfDisplayRegular.name, 34.0, 41.0, 0)
+		public static let displayRegular = Self(SFProFont.sfDisplayRegular.name, 34.0, 41.0, 0)
 		public static let displayBold = Self(SFProFont.sfBold.name, 34, 41.0, 0)
-
-		public static let displayDisplayRegularItalic = Self(SFProFont.sfDisplayRegularItalic.name, 48, 3.6, -0.4)
-		public static let displayBlack = Self(SFProFont.sfBlack.name, 56, 4.2, -0.4)
-		public static let displayBlackItalic = Self(SFProFont.sfBlackItalic.name, 48, 3.6, -0.4)
-		public static let displayBoldItalic = Self(SFProFont.sfBoldItalic.name, 48, 3.6, -0.4)
-		public static let displayDisplayHeavy = Self(SFProFont.sfDisplayHeavy.name, 48, 3.6, -0.4)
-		public static let displayDisplayHeavyItalic = Self(SFProFont.sfDisplayHeavyItalic.name, 48, 3.6, -0.4)
-		public static let displayDisplayLight = Self(SFProFont.sfDisplayLight.name, 48, 3.6, -0.4)
-		public static let displayDisplayLightItalic = Self(SFProFont.sfDisplayLightItalic.name, 48, 3.6, -0.4)
-		public static let displayDisplayMedium = Self(SFProFont.sfDisplayMedium.name, 48, 3.6, -0.4)
-		public static let displayDisplayMediumItalic = Self(SFProFont.sfDisplayMediumItalic.name, 48, 3.6, -0.4)
-		public static let displayDisplaySemiBold = Self(SFProFont.sfDisplaySemiBold.name, 48, 3.6, -0.4)
-		public static let displayDisplaySemiBoldItalic = Self(SFProFont.sfDisplaySemiBoldItalic.name, 48, 3.6, -0.4)
-		public static let displayDisplayThin = Self(SFProFont.sfDisplayThin.name, 48, 3.6, -0.4)
-		public static let displayDisplayThinItalic = Self(SFProFont.sfDisplayThinItalic.name, 48, 3.6, -0.4)
-		public static let displayDisplayUltraLight = Self(SFProFont.sfDisplayUltraLight.name, 48, 3.6, -0.4)
-		public static let displayDisplayUltraLightItalic = Self(SFProFont.sfDisplayUltraLightItalic.name, 48, 3.6, -0.4)
+		public static let heading1 = Self(SFProFont.sfDisplayRegular.name, 28.0, 36.0, 0)
+		public static let heading1Bold = Self(SFProFont.sfBold.name, 28.0, 36.0, 0)
+		public static let heading2 = Self(SFProFont.sfDisplayRegular.name, 24.0, 31.0, 0)
+		public static let heading2Bold = Self(SFProFont.sfBold.name, 24.0, 31.0, 0)
+		public static let heading3 = Self(SFProFont.sfDisplayRegular.name, 20.0, 26.0, 0)
+		public static let heading3Bold = Self(SFProFont.sfBold.name, 20.0, 26.0, 0)
+		public static let heading4 = Self(SFProFont.sfDisplayRegular.name, 16.0, 21.0, 0)
+		public static let heading4SemiBold = Self(SFProFont.sfDisplaySemiBold.name, 16.0, 21.0, 0)
+		public static let heading5 = Self(SFProFont.sfDisplayRegular.name, 14.0, 18.0, 0)
+		public static let heading5SemiBold = Self(SFProFont.sfDisplayRegular.name, 14.0, 18.0, 0)
 
 		func dynamicFontSize(for category: ContentSizeCategory) -> CGFloat {
 			let font = UIFont(name: fontName, size: fontSize)
