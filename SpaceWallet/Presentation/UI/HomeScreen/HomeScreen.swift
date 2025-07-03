03bca3defd955d81658c0109c385d4aa5f52d6f6
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-03 10:40:35 +0100

    Cleanup.

--- SpaceWallet/Presentation/UI/HomeScreen/HomeScreen.swift
+++ SpaceWallet/Presentation/UI/HomeScreen/HomeScreen.swift
@@ -15,7 +15,7 @@ public struct HomeScreen: View {
 	private var onAction: (ActionType) -> Void
 
 	public init(
-		tabSelection: TabBarItem = .init(icon: .tabHomeSelected, title: "Home"),
+		tabSelection: TabBarItem = .init(icon: .tabHomeSelected, title: TabBarId.home.id),
 		onAction: @escaping (ActionType) -> Void
 	) {
 		self.tabSelection = tabSelection
@@ -25,20 +25,38 @@ public struct HomeScreen: View {
 	public var body: some View {
 		TabBar(selection: $tabSelection) {
 			EmptyView()
-				.tabBarItem(selectedIcon: .tabHomeSelected, unselectedIcon: .tabHomeDeselected, title: "Home")
+				.tabBarItem(selectedIcon: .tabHomeSelected, unselectedIcon: .tabHomeDeselected, title: TabBarId.home.id)
 			EmptyView()
-				.tabBarItem(selectedIcon: .tabWalletSelected, unselectedIcon: .tabWalletDeselected, title: "Wallet")
+				.tabBarItem(selectedIcon: .tabWalletSelected, unselectedIcon: .tabWalletDeselected, title: TabBarId.wallet.id)
 			EmptyView()
-				.tabBarItem(selectedIcon: .tabCreditCardSelected, unselectedIcon: .tabCreditCardDeselected, title: "Credit")
+				.tabBarItem(selectedIcon: .tabCreditCardSelected, unselectedIcon: .tabCreditCardDeselected, title: TabBarId.credit.id)
 			EmptyView()
-				.tabBarItem(selectedIcon: .tabSearchSelected, unselectedIcon: .tabSearchDeselected, title: "Search")
+				.tabBarItem(selectedIcon: .tabSearchSelected, unselectedIcon: .tabSearchDeselected, title: TabBarId.search.id)
 			EmptyView()
-				.tabBarItem(selectedIcon: .tabStarsSelected, unselectedIcon: .tabStarsDeselected, title: "Stars")
+				.tabBarItem(selectedIcon: .tabStarsSelected, unselectedIcon: .tabStarsDeselected, title: TabBarId.stars.id)
 		}
 	}
 }
 
 public extension HomeScreen {
+
+	enum TabBarId: String {
+		case home
+		case wallet
+		case credit
+		case search
+		case stars
+
+		public var id: String {
+			switch self {
+				case .home: return "Home"
+				case .wallet: return "Wallet"
+				case .credit: return "Credit"
+				case .search: return "Search"
+				case .stars: return "Stars"
+			}
+		}
+	}
 
 	enum ActionType {
 		case receive
