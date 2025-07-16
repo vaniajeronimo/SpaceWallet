dfbb43d28776a3007e76d421c7681c5e7ef8f710
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-16 19:09:14 +0100

    Improve tabBar.

--- SpaceWallet/Presentation/Common/TabBar/TabBar.swift
+++ SpaceWallet/Presentation/Common/TabBar/TabBar.swift
@@ -15,6 +15,13 @@ public struct TabBar: View {
 	@Binding private var selection: TabBarItem
 	@State private var tabs: [TabBarItem]
 
+	private var safeAreaBottomInset: CGFloat {
+		UIApplication.shared.connectedScenes
+			.compactMap { ($0 as? UIWindowScene)?.keyWindow }
+			.first?
+			.safeAreaInsets.bottom ?? 0
+	}
+
 	public init(selection: Binding<TabBarItem>, @TabsBuilder content: () -> [Tab]) {
 		self._selection = selection
 		self.content = content()
@@ -34,45 +41,62 @@ public struct TabBar: View {
 			}
 			tabBarItems()
 		}
-		.background(Color.b0
-			.blur(radius: 32)
-		)
 		.onAppear {
 			UITabBar.appearance().isHidden = true
 		}
 	}
 
 	private func tabBarItems() -> some View {
-		HStack(alignment: .bottom, spacing: 10) {
-			ForEach(Array(tabs.enumerated()), id: \.offset) { index, tab in
-				Button {
-					if selection == tab, let navigation = content[safe: index]?.content as? Navigation {
-						navigation.navigation.popToRoot()
+		VStack(spacing: .zero) {
+			HStack(alignment: .bottom, spacing: 10) {
+				ForEach(Array(tabs.enumerated()), id: \.offset) { index, tab in
+					Button {
+						if selection == tab, let navigation = content[safe: index]?.content as? Navigation {
+							navigation.navigation.popToRoot()
+						}
+						selection = tab
+					} label: {
+						tabButton(tab: tab)
 					}
-					selection = tab
-				} label: {
-					tabButton(tab: tab)
+					.buttonStyle(PlainButtonStyle())
 				}
-				.buttonStyle(PlainButtonStyle())
 			}
+			.padding(.horizontal, UI.Spacing.level06)
 		}
-		.ignoresSafeArea(edges: .bottom)
-		.frame(maxWidth: .infinity)
-		.padding(.horizontal, UI.Spacing.level06)
+		.frame(maxWidth: .infinity, maxHeight: 30)
+		.padding(.bottom, safeAreaBottomInset)
+		.background(tabBarBackground)
+		.edgesIgnoringSafeArea(.bottom)
+	}
+
+	private var tabBarBackground: some View {
+		Rectangle()
+			.fill(Color.white.opacity(0.05))
+			.background(.ultraThinMaterial)
+			.overlay(
+				Rectangle()
+					.stroke(Color.white, lineWidth: 1)
+			)
+			.edgesIgnoringSafeArea(.bottom)
 	}
 
 	private func tabButton(tab: TabBarItem) -> some View {
-		VStack(spacing: 7) {
+		VStack(spacing: 4) {
 			let isSelected = selection == tab
-			let iconImage = isSelected ? tab.selectedIcon : tab.unselectedIcon
+			let iconImage = isSelected
+			? tab.selectedIcon?.renderingMode(.original)
+			: tab.unselectedIcon?.renderingMode(.template)
+
 			iconImage?
 				.resizable()
 				.scaledToFit()
 				.frame(width: 67, height: 44)
 				.animation(nil, value: selection)
+				.if(!isSelected) { unselectedIcon in
+					unselectedIcon.foregroundStyle(.grayPrimary)
+				}
 		}
-		.ignoresSafeArea(edges: .bottom)
-		.padding(.bottom, 44)
-		.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottom)
+		.padding(.top, 44)
+		.frame(maxWidth: .infinity)
 	}
 }
