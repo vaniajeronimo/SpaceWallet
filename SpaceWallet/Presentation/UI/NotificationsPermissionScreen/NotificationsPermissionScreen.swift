a41e18852c269b82bc914ac876a1574f01cf4e90
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-22 11:06:29 +0100

    UI fixes.

--- SpaceWallet/Presentation/UI/NotificationsPermissionScreen/NotificationsPermissionScreen.swift
+++ SpaceWallet/Presentation/UI/NotificationsPermissionScreen/NotificationsPermissionScreen.swift
@@ -16,7 +16,18 @@ public struct NotificationsPermissionScreen: View {
 	}
 
 	public var body: some View {
-		ZStack {
+		VStack {
+			content
+			cta
+		}
+		.padding(.horizontal, UI.Spacing.level07)
+		.background(.textPrimary)
+	}
+
+	private var content: some View {
+		ScrollView(showsIndicators: false) {
+			Spacer(minLength: 50)
+
 			VStack(spacing: .zero) {
 				Image.onboarding_notifications
 					.resizable()
@@ -28,15 +39,26 @@ public struct NotificationsPermissionScreen: View {
 						.font(.heading2Bold)
 						.foregroundColor(.b0)
 
-					Text("notifications_permission_description".localized)
+					Text("notifications_permission_subtitle".localized)
 						.font(.body)
 						.fontWeight(.medium)
 						.foregroundColor(.textTertiary)
 				}
 				.multilineTextAlignment(.center)
 			}
+			.padding(.top, UI.Spacing.level08)
+
+			Spacer(minLength: 50)
 		}
-		.background(.textPrimary)
-		.ignoresSafeArea()
+	}
+
+	private var cta: some View {
+		Button {
+			onAction()
+		} label: {
+			Text("enable".localized)
+		}
+		.buttonStyle(PrimaryButton(.large, color: .b0, titleColor: .textPrimary))
+		.padding(.bottom, UI.Spacing.level06)
 	}
 }
