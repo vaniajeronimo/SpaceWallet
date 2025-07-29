204af2cdd1d073ebe9f013cedc737c0ba4520ef6
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-29 17:43:32 +0100

    Fix localizable strings.

--- SpaceWallet/Presentation/UI/DiscoveryScreen/DiscoveryScreenViewModel.swift
+++ SpaceWallet/Presentation/UI/DiscoveryScreen/DiscoveryScreenViewModel.swift
@@ -13,11 +13,11 @@ extension DiscoveryScreen {
 	final class ViewModel {
 
 		var nfts: [CollectibleModel] = [
-			.init(name: "purple_monkey", imageName: "purple_monkey_nft", description: "space_monkey_nft_description".localized),
-			.init(name: "dark_monkey_nft", imageName: "dark_monkey_nft", description: "orange_monkey_nft_description".localized),
-			.init(name: "gray_monkey", imageName: "gray_monkey_nft", description: "banana_monkey_nft_description".localized),
-			.init(name: "dark_purple_monkey_nft", imageName: "dark_purple_monkey_nft", description: "militar_monkey_nft".localized),
-			.init(name: "white_monkey_nft", imageName: "white_monkey_nft", description: "dark_spectrum_nft_description".localized)
+			.init(name: "purple_monkey", imageName: "purple_monkey_nft", description: "purple_monkey_nft_description".localized),
+			.init(name: "dark_monkey", imageName: "dark_monkey_nft", description: "dark_monkey_nft_description".localized),
+			.init(name: "gray_monkey", imageName: "gray_monkey_nft", description: "gray_monkey_nft_description".localized),
+			.init(name: "dark_purple_monkey", imageName: "dark_purple_monkey_nft", description: "dark_purple_monkey_nft_description".localized),
+			.init(name: "white_monkey", imageName: "white_monkey_nft", description: "white_monkey_nft_description".localized)
 		]
 
 		init() { }
