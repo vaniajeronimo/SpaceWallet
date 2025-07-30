6f46386a04bbdf13b21bd67afac937a2e3cb70d3
vaniajeronimo <vania.jeronimo@gmail.com>
2025-07-30 09:39:02 +0100

    Update nfts description.

--- SpaceWallet/Presentation/UI/DiscoveryScreen/DiscoveryScreenViewModel.swift
+++ SpaceWallet/Presentation/UI/DiscoveryScreen/DiscoveryScreenViewModel.swift
@@ -14,9 +14,9 @@ extension DiscoveryScreen {
 
 		var nfts: [CollectibleModel] = [
 			.init(id: 1, name: "purple_monkey", imageName: "purple_monkey_nft", description: "purple_monkey_nft_description".localized),
-			.init(id: 2, name: "dark_monkey", imageName: "dark_monkey_nft", description: "dark_monkey_nft_description".localized),
-			.init(id: 3, name: "gray_monkey", imageName: "gray_monkey_nft", description: "gray_monkey_nft_description".localized),
-			.init(id: 4, name: "dark_purple_monkey", imageName: "dark_purple_monkey_nft", description: "dark_purple_monkey_nft_description".localized),
+			.init(id: 2, name: "dark_purple_monkey", imageName: "dark_purple_monkey_nft", description: "dark_purple_monkey_nft_description".localized),
+			.init(id: 3, name: "dark_monkey", imageName: "dark_monkey_nft", description: "dark_monkey_nft_description".localized),
+			.init(id: 4, name: "gray_monkey", imageName: "gray_monkey_nft", description: "gray_monkey_nft_description".localized),
 			.init(id: 5, name: "white_monkey", imageName: "white_monkey_nft", description: "white_monkey_nft_description".localized)
 		]
 
